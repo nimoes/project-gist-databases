3384e9bd7ed1b23836c08f9582372c27fc39ec8c
Min Seo <mhseo2180@gmail.com>
2018-08-04 21:39:06 +0000

    prior to submitting

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,42 @@
 import requests
 
+
 def import_gists_to_database(db, username, commit=True):
-    pass
+    """
+    Your import_gists_to_database function will take three parameters:
+    db: The database object to connect to
+    username: The GitHub user whose gists we are going to retrieve
+    commit (Optional, defaults to True): If True, automatically commit changes to database
+    You are going to use the GitHub gists API to retrieve the gists of a given user, insert
+    those gists into a database (schema may be found in the schema.sql file), and if commit is True,
+    commit those changes to the database.
+    """
+
+    query = "INSERT INTO gists('github_id', 'html_url', 'git_pull_url', 'git_push_url', 'commits_url', \
+    'forks_url', 'public', 'created_at', 'updated_at', 'comments', 'comments_url') \
+    VALUES (:github_id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url, \
+    :public, :created_at, :updated_at, :comments, :comments_url)"
+
+    r = requests.get('https://api.github.com/users/{}/gists'.format(username))
+    r.raise_for_status()
+
+    gists_data = r.json()
+
+    for gist in gists_data:
+        db.execute(query, 
+        {
+            'github_id': gist['id'],
+            'html_url': gist['html_url'],
+            'git_pull_url': gist['git_pull_url'],
+            'git_push_url': gist['git_push_url'],
+            'commits_url': gist['commits_url'],
+            'forks_url': gist['forks_url'],
+            'public': gist['public'],
+            'created_at': gist['created_at'],
+            'updated_at': gist['updated_at'],
+            'comments': gist['comments'],
+            'comments_url': gist['comments_url']
+        })
+        
+        if commit:
+            db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,56 @@
 from .models import Gist
+from datetime import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    """
+    Your search_gists method should take a db_connection parameter (the database connection), 
+    as well as two optional arguments:
+        github_id
+        created_at
+    If no parameter is provided, all the gists in the database should be returned. 
+    If public_id or created_at parameters are provided, you should filter your SELECT query based on them.
+    """
+    
+    query = "SELECT * FROM gists "
+    
+    if not kwargs:
+        return db_connection.execute(query).fetchall()
+    
+    for key,val in kwargs.items():
+        if key == 'github_id':
+            query += "WHERE github_id = :github_id"
+            # data with given specifics retrieved in a tuple
+            tup = db_connection.execute(query, {'github_id': val})
+        elif key == 'created_at':
+            # 2014-05-03T20:26:08Z
+            # d = datetime(2014, 5, 3, 20, 26, 8)
+            # date = datetime.strptime(str(val), "%Y-%m-%d %H:%M:%S")
+            query += "WHERE datetime(created_at) = :created_at"
+            tup = db_connection.execute(query, {'created_at': val})
+    # before returning, convert tup into Gist models or list of gist objects
+    result = []
+    for eachitem in tup:
+        result.append(Gist(eachitem))
+    return result
+
+    
+    """
+    This project contains optional tests that you can just uncomment if you want an extra challenge. 
+    Your task will be to extend the search_gists function to accept the following optional parameters:
+
+        created_at__gt
+        created_at__gte
+        created_at__lt
+        created_at__lte
+        updated_at__gt
+        updated_at__gte
+        updated_at__lt
+        updated_at__lte
+
+    These parameters will be operating against the created_at and updated_at fields using 
+    the corresponding comparison: gt means greater than, gte greater than or equal to, 
+    lt less than, lte less than or equal to.
+
+    That is, created_at__gt=datetime(2018, 1, 1) are all the Gists that were created 
+    AFTER January 1st 2018. If we use created_at__gt, that’d also include the corresponding day.
+    """
